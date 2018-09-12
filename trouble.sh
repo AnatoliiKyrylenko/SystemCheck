9bedea9f268b1baa7c48281509cea3228f35d7dd
AnatoliiKyrylenko <KyrylenkoAnatolii@gmail.com>
2018-09-12 22:40:53 +0300

    check main daemon

---
+++ trouble.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for service in sshd apache2 nginx mysql mariadb httpd crond firewalld ufw; do
+systemctl status $service | grep --quiet "loaded"
+if [ $? -eq 0 ]; then
+echo -en $service "\033[0;32m [active (exited)] \033[0m";
+else
+echo -en $service "\033[0;31m inactive (dead) \033[0m";
+fi
+done
+echo ""
