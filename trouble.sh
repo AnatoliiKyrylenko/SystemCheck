3abd91ec8d03c2ca3ef4dc2ac519de50dc96fc42
AnatoliiKyrylenko <KyrylenkoAnatolii@gmail.com>
2018-09-13 22:37:19 +0300

    add cron status

--- trouble.sh
+++ trouble.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-for service in sshd apache2 nginx mysql mariadb httpd crond firewalld ufw; 
+echo "--------------------------"
+for service in sshd apache2 nginx mysql mariadb httpd cron crond firewalld ufw; 
 do
 systemctl status $service | grep --quiet "loaded"
-	if [ $? -eq 0 ]; then
-		echo -en $service "\033[0;32m active (exited) \033[0m\n"
-	else
-		echo -en $service "\033[0;31m inactive (dead) \033[0m\n"
-	fi
+        if [ $? -eq 0 ]; then
+                echo -en $service "\033[0;32m active (exited) \033[0m\n"
+        else
+                echo -en $service "\033[0;31m inactive (dead) \033[0m\n"
+        fi
 done
+echo "--------------------------"
+
